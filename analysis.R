f7f7f4aea5f41261b431dcd9cd84c3e99ea63cbd
Sarah Kolbe <kolbe.sarah@gmail.com>
2024-08-01 01:56:07 +0000

    Basic map

--- analysis.R
+++ analysis.R
@@ -11,52 +11,49 @@ options(scipen = 999)
 
 # Load data ----------
 
-dist_finance <- read_csv("source_data/compcost_sum_1415_to_2223.csv")
+finance <- read_csv("source_data/compcost_sum_1415_to_2223.csv")
 
-elsd <- read_sf("source_data/tl_2023_55_elsd/tl_2023_55_elsd.shp")
-scsd <- read_sf("source_data/tl_2023_55_scsd/tl_2023_55_scsd.shp")
-unsd <- read_sf("source_data/tl_2023_55_unsd/tl_2023_55_unsd.shp")
+geography <- read_sf("source_data/Wisconsin_School_Districts/WI_School_Districts.shp")
 
 # Clean and prep data ----------
 
-districts <- dist_finance %>%
+# Clean up column names
+colnames(finance) <- tolower(str_replace_all(colnames(finance), " ", "_"))
+
+# Format and add basic aggregations for finance data
+dist_finance <- finance %>%
   pivot_longer(cols = c(starts_with("member"), starts_with("instruct"), starts_with("pupil"),
                         starts_with("admin"), starts_with("operat"), starts_with("trans"),
                         starts_with("facil"), starts_with("food")),
                 names_to = c('.value', 'fiscal_year'),
                 names_pattern = '(.*)_([0-9]{4})$') %>%
-  rename(dist_code = DISTRICT_CODE,
-         dist_name = DISTRICT_NAME,
-         support = pupilstaffsupport,
+  rename(support = pupilstaffsupport,
          food = foodcommservice) %>%
   # Create fields for Total Current Educational Cost (TCEC), Total Education Cost (TEC), and Total District Cost (TDC)
   mutate(tcec = select(., instruction:operations) %>% rowSums(na.rm = TRUE),
          tec = select(., instruction:facility) %>% rowSums(na.rm = TRUE),
          tdc = select(., instruction:food) %>% rowSums(na.rm = TRUE)) %>%
   # Format district code
-  mutate(dist_code = str_pad(dist_code, 4, side = c("left"), pad = "0")) %>%
+  mutate(district_code = str_pad(district_code, 4, side = c("left"), pad = "0")) %>%
   # Filter out state totals
-  filter(dist_code != 9999)
+  filter(district_code != 9999)
 
-# Create statewide shapefiles for elementary+unified and secondary+unified school districts
-elun <- bind_rows(elsd, unsd) %>%
+# Clean up columnn names in geography file
+dist_geo <- geography %>%
   rename_all(tolower) %>%
-  rename(dist_code)
-scun <- bind_rows(scsd, unsd) %>%
-  rename_all(tolower)
-rm(elsd, scsd, unsd)
+  rename(district_code = sdid)
 
 # Join finance and spatial data ----------
 
-elun_finance <- left_join(elun, districts, by = "dist_code")
+# Check for districts present in the geography but missing in the finance data
+missing <- anti_join(dist_geo, dist_finance, by="district_code")
+
+# Join geographic and finance data
+district <- left_join(dist_geo, dist_finance, by = "district_code")
 
 # Plot ----------
-
+selected_dist <- filter(district, fiscal_year == 2023)
 ggplot() +
-  geom_sf(data = elsd_unsd) +
-  theme_minimal()
-
-ggplot() +
-  geom_sf(data = scsd_unsd) +
+  geom_sf(data = selected_dist) +
   theme_minimal()
 
