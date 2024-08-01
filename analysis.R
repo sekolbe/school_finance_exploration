d392ffb72932cf054b3e05cce321d9c16b608bda
Sarah Kolbe <kolbe.sarah@gmail.com>
2024-08-01 19:08:28 +0000

    Add a gif of TCEC

--- analysis.R
+++ analysis.R
@@ -1,4 +1,4 @@
-# Simple cleaning and exploration of WI school finance and census data
+# Simple cleaning and exploration of WI school finance data
 # S. Kolbe
 # Updated 2024.07.31
 
@@ -6,6 +6,8 @@
 
 library(tidyverse)
 library(sf)
+library(paletteer)
+library(gganimate)
 
 options(scipen = 999)
 
@@ -22,26 +24,28 @@ colnames(finance) <- tolower(str_replace_all(colnames(finance), " ", "_"))
 
 # Format and add basic aggregations for finance data
 dist_finance <- finance %>%
-  pivot_longer(cols = c(starts_with("member"), starts_with("instruct"), starts_with("pupil"),
-                        starts_with("admin"), starts_with("operat"), starts_with("trans"),
+  pivot_longer(cols = c(starts_with("member"), starts_with("instruct"), 
+                        starts_with("pupil"), starts_with("admin"), 
+                        starts_with("operat"), starts_with("trans"),
                         starts_with("facil"), starts_with("food")),
                 names_to = c('.value', 'fiscal_year'),
                 names_pattern = '(.*)_([0-9]{4})$') %>%
   rename(support = pupilstaffsupport,
          food = foodcommservice) %>%
-  # Create fields for Total Current Educational Cost (TCEC), Total Education Cost (TEC), and Total District Cost (TDC)
-  mutate(tcec = select(., instruction:operations) %>% rowSums(na.rm = TRUE),
-         tec = select(., instruction:facility) %>% rowSums(na.rm = TRUE),
-         tdc = select(., instruction:food) %>% rowSums(na.rm = TRUE)) %>%
+  # Create fields for Total Current Educational Cost (TCEC), Total Education Cost (TEC), and Total District Cost (TDC) per pupil
+  mutate(tcec = select(., instruction:operations) %>% rowSums(na.rm = TRUE)/member,
+         tec = select(., instruction:facility) %>% rowSums(na.rm = TRUE)/member,
+         tdc = select(., instruction:food) %>% rowSums(na.rm = TRUE)/member) %>%
   # Format district code
   mutate(district_code = str_pad(district_code, 4, side = c("left"), pad = "0")) %>%
   # Filter out state totals
   filter(district_code != 9999)
 
-# Clean up columnn names in geography file
+# Clean up columnn names in geography file and reproject
 dist_geo <- geography %>%
   rename_all(tolower) %>%
-  rename(district_code = sdid)
+  rename(district_code = sdid) %>%
+  st_transform(., 'EPSG:3070')
 
 # Join finance and spatial data ----------
 
@@ -51,9 +55,21 @@ missing <- anti_join(dist_geo, dist_finance, by="district_code")
 # Join geographic and finance data
 district <- left_join(dist_geo, dist_finance, by = "district_code")
 
-# Plot ----------
-selected_dist <- filter(district, fiscal_year == 2023)
-ggplot() +
-  geom_sf(data = selected_dist) +
-  theme_minimal()
+# Make a gif of TCEC per pupil ----------
 
+# Create frames
+tcec_frames <- ggplot() + 
+  # plot the state data
+  geom_sf(data = district, aes(fill = tcec)) +
+  scale_fill_paletteer_c("grDevices::Purple-Yellow", direction = -1) +
+  labs(title = "Total Current Educational Cost (TCEC) Per Pupil", 
+       subtitle = '{current_frame}',
+       fill = "TCEC Per Pupil",
+       caption = "Data from Wisconsin Department of Public Instruction") + 
+  theme_minimal() +
+  theme(legend.box.background = element_rect())+
+  transition_manual(fiscal_year)
+
+# Animate frames and save
+animate(tcec_frames, fps = 10)
+anim_save("output/Wisconsin TCEC per pupil 2015-2023.gif")
